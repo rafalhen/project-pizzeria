18b7566fd493f46d7890c6e5e4ac6d07269c9327
Rafal Hen <rafalhen@poczta.onet.pl>
2020-12-07 21:16:53 +0100

    accordon

--- src/js/script.js
+++ src/js/script.js
@@ -92,18 +92,17 @@
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventlistener('click', function(event) {
+      clickableTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
         event.preventDefault();
 
         /* find active product (product that has active class) */
-        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        for(let activeProduct of activeProducts) {
-          if (activeProduct !== null && activeProduct !== thisProduct.element){
-            activeProduct.classList.remove('active');
-          }
+
+        if (activeProduct !== null && activeProduct !== thisProduct.element){
+          activeProduct.classList.remove('active');
         }
 
         /* toggle active class on thisProduct.element */
